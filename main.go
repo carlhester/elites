0d79e8d8806240270187fd084bef18cdf9beeec6
Carl Hester <crucialcarl@gmail.com>
2020-10-27 20:56:38 -0700

    framework

--- main.go
+++ main.go
@@ -1,69 +1,79 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-
 type player struct {
-    Name string
-    attribs map[string]Attrib
+	Name    string
+	attribs map[string]Attrib
 }
 
 type Attrib interface {
-    Get()
+	Get()
 }
 
-func (p *player)AddAttrib(name string, attrib Attrib) {
-    p.attribs[name] = attrib
+func (p *player) AddAttrib(name string, attrib Attrib) {
+	p.attribs[name] = attrib
 }
 
-func (p *player)HasAttrib(name string) bool{
-    if _, ok := p.attribs[name]; ok  {
-        return true
-    }
-    return false
+func (p *player) HasAttrib(name string) bool {
+	if _, ok := p.attribs[name]; ok {
+		return true
+	}
+	return false
 }
 
-func (p *player)RemoveAttrib(attrib string) { 
-    _, ok := p.attribs[attrib]
-    if ok { 
-        delete(p.attribs, attrib)
-    }
+func (p *player) RemoveAttrib(attrib string) {
+	_, ok := p.attribs[attrib]
+	if ok {
+		delete(p.attribs, attrib)
+	}
 }
 
 type health struct {
-    hp int
+	hp int
 }
 
-func (h health)Get() {}
+func (h health) Get() {}
+
+type heal struct {
+	hp int
+}
+
+func (h heal) Get() {}
 
 type game struct {
-    p1  *player
-    p2  *player
-    turn int
+	p1   *player
+	p2   *player
+	turn int
 }
 
 func main() {
-    var players []*player
-    p1 := &player{Name: "P1", attribs: make(map[string]Attrib)}
-    p2 := &player{Name: "P2", attribs: make(map[string]Attrib)}
+	var players []*player
+	p1 := &player{Name: "P1", attribs: make(map[string]Attrib)}
+	p2 := &player{Name: "P2", attribs: make(map[string]Attrib)}
 
-    p1.AddAttrib("health", health{hp: 10})
-    p2.AddAttrib("health", health{hp: 10})
-    players = append(players, p1, p2)
+	p1.AddAttrib("health", health{hp: 10})
+	p2.AddAttrib("health", health{hp: 10})
+	players = append(players, p1, p2)
 
-    //g := &game{p1: p1, p2: p2,}
+	for turn := 0; turn < 10; turn++ {
+		for _, p := range players {
+			p.AddAttrib("heal", heal{hp: 5})
+			DoHeal(p)
+			fmt.Printf("%+v\n", p)
+		}
+	}
+}
 
-    for turn := 0; turn < 10; turn++ {
-       // g.ShowStatus()
-       for _, p := range players{
-           fmt.Println(p.Name)
-       // P1Turn()
-       // ShowStatus()
-       // P2Turn()
-
-        }
-    }
-
+func DoHeal(p *player) {
+	if p.HasAttrib("heal") {
+		if p.HasAttrib("health") {
+			newHp := p.attribs["health"].(health).hp + p.attribs["heal"].(heal).hp
+			p.RemoveAttrib("heal")
+			p.RemoveAttrib("health")
+			p.AddAttrib("health", health{hp: newHp})
+		}
+	}
 }
