2738f95ece05d86662b9c84f5825c63f8f8fed61
Carl Hester <crucialcarl@gmail.com>
2020-10-28 21:34:56 -0700

    rework

--- main.go
+++ main.go
@@ -1,61 +1,85 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-type move struct{
-    name string
-    movetype string
-    value int
+type move struct {
+	name  string
+	value int
+    moveType string
 }
-
-func (m move)execute(p *player) { 
-    
-}
-
 
 type player struct {
-	name    string
-    health int
-    moves []move
+	name     string
+	hp       int
+	moves    []move
+	nextMove move
+    enemy *player
 }
 
+func (p *player) doNextMove() {
+    if p.nextMove.moveType == "heal" {
+       p.hp += p.nextMove.value
+       return
+    }
+    if p.nextMove.moveType == "attack" {
+        p.enemy.hp -= p.nextMove.value
+        return
+    }
+}
 
 type game struct {
-	p1   *player
-	p2   *player
-	turn int
+	players []*player
+	turn    int
 }
 
-func (p player)showOpts() { 
-    fmt.Println(p.moves)
+func (g game) showStatus() {
+    fmt.Println("================")
+	for _, g := range g.players {
+        fmt.Println(g.name,":", g.hp)
+	}
 }
 
+func (p player) showOpts() {
+	for i := range p.moves {
+		fmt.Println(i, p.moves[i].name)
+	}
+}
+
+func (p *player) handleInput(cmd int) {
+    p.nextMove = p.moves[cmd]
+}
 
 func main() {
 	var players []*player
-    healme := move{name: "healme", value: 3, movetype: "HEAL"}
-    p1 := &player{name: "P1", health: 100, moves: []move{healme}}
+    healme := move{name: "healme", value: 3, moveType: "heal"}
+    attack := move{name: "attack", value: 2, moveType: "attack"}
+    p1 := &player{name: "P1", hp: 100, moves: []move{healme, attack}}
+    p2 := &player{name: "P2", hp: 100, moves: []move{healme, attack}, enemy: p1}
+    p1.enemy = p2
 
-	players = append(players, p1)
+	g := &game{}
+	g.players = append(players, p1, p2)
 
 	for turn := 0; turn < 10; turn++ {
-		for _, p := range players {
-            p.showOpts() 
-            cmd := GetInput()
-            fmt.Println(cmd)
+		for _, p := range g.players {
+			g.showStatus()
+			p.showOpts()
+			p.handleInput(p.GetInput())
+            p.doNextMove()
 		}
 	}
 }
 
-func GetInput() int {
+func (p player)GetInput() int {
+    fmt.Println(p.name, "'s turn:  ")
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	inputChar := input[0:1]
-    val, _ := strconv.Atoi(inputChar)
-    return val
+	val, _ := strconv.Atoi(inputChar)
+	return val
 }
